91bf83718c5adf6ced71eed6943b0907bdcd0f00
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2022-12-11 21:35:49 +0200

    code review js

--- Fylo-FronedMentor-Challenge/script.js
+++ Fylo-FronedMentor-Challenge/script.js
@@ -1,58 +1,64 @@
-let totalSize = 10;
-var sizeOccupied = 0;
-var percent;
-var sizeLeft;
+const totalSize = 10;
+let usedSize = 0;
+let percent;
+let sizeLeft;
+totalSizeElement = document.getElementById("totalSize");
+sizeLeftElement = document.getElementById("sizeLeft");
+progressElement = document.getElementById("progress");
+usedSizeElement = document.getElementById("sizeOccupied");
+
+
+const init = () => {
+    usedSize = Number(window.localStorage.getItem('usedSize'));
+    percent = 100 * (usedSize / totalSize) + 4;
+    sizeLeft = totalSize - usedSize;
+    totalSizeElement.innerText = totalSize + " MB";
+    sizeLeftElement.innerText = shorten(sizeLeft);
+    usedSizeElement.innerText = shorten(usedSize) + " MB";
+    progressElement.style.width = percent.toString(10) + "%";
+}
+
+const shorten = (x) => {
+    return Number.parseFloat(x).toFixed(2);
+}
+
+const addSize = (s) => {
+    s /= (Math.pow(1024, 2));
+    if (usedSize + s < totalSize) {
+        usedSize += s;
+        sizeLeft = totalSize - usedSize;
+        percent = 100 * (usedSize / totalSize) - 4;
+        usedSizeElement.innerText = shorten(usedSize) + " MB";
+        sizeLeftElement.innerText = shorten(sizeLeft);
+        progressElement.style.width = percent.toString(10) + "%";
+        progressElement.style.transition = "width 0.5s ease 0.1s";
+    } else {
+        alert("There is not enough space on the disk")
+    }
+}
+
+
+const onFileInputChange = (e) => {
+    const fileName = e.value;
+    const isImgFile = new RegExp('(\.(gif|jpeg|jpg|png))').test(fileName);
+    if (isImgFile) {
+        const file = e.files[0];
+        addSize(file.size)
+    } else {
+        alert("File Type Not Supported");
+    }
+}
+
+var clear = document.getElementById('clear');
+clear.addEventListener("click", () => {
+    usedSize = 0;
+    init();
+});
+
+//main start
 
 init();
 
-function init(){
-   // sizeOccupied = localStorage.sizeO;
-   var percent = 100 * (sizeOccupied/totalSize) + 4;
-   var sizeLeft = totalSize-sizeOccupied;
-   document.getElementById("totalSize").innerText = totalSize + " MB";
-   document.getElementById("sizeLeft").innerText = financial(sizeLeft);
-   document.getElementById("sizeOccupied").innerText = financial(sizeOccupied)  + " MB";
-   document.getElementById("size-left-type").innerText = "MB Left"
-   document.getElementById("progress").style.width = percent.toString(10)  + "%";
-}
 
-function financial(x) {
-   return Number.parseFloat(x).toFixed(2);
-}
 
-function addSize(s){
-   s/= (Math.pow(1024,2));
-   sizeOccupied += s;
-   if (sizeOccupied < totalSize){
-      sizeLeft = totalSize-sizeOccupied;
-      percent = 100 * (sizeOccupied/totalSize) - 4;
-      document.getElementById("sizeOccupied").innerText = financial(sizeOccupied)  + " MB";
-      document.getElementById("sizeLeft").innerText = financial(sizeLeft);
-      document.getElementById("progress").style.width = percent.toString(10) + "%";
-      document.getElementById("progress").style.transition = "width 0.5s ease 0.1s";
-   }
-   else {
-      sizeOccupied -= s;
-      alert("There is not enough space on the disk")
-   }
-}
 
-var uploadBtn = document.getElementById('choose-file');
-uploadBtn.addEventListener("change",() =>{
-   const pattern = new RegExp('(\.(gif|jpe?g|jpg|png))');
-   if (pattern.test(uploadBtn.files[0].name)){
-      const file = uploadBtn.files[0];
-      addSize(file.size)
-      uploadBtn.files[0].name = "";
-   }
-   else{
-      alert("File Type Not Supported");
-   }
-});
-
-var clear = document.getElementById('clear');
-clear.addEventListener("click",() => {
-   // localStorage.setItem('sizeO', 0);
-   sizeOccupied = 0;
-   init();
-});
